46832e76cac7c4c75f3022faa5d80fc5002f57d0
itmoti <motiarrahman9707@gmail.com>
2022-10-12 01:08:44 +0600

    home section finished

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link, useLoaderData } from 'react-router-dom';
+import {  useLoaderData } from 'react-router-dom';
+import './Home.css'
 
 import Subject from '../Subject/Subject';
 
@@ -7,19 +8,40 @@ const Home = () => {
     const topics = useLoaderData();
     const subjects = topics.data;
 
-    // const handleQuesButton = (id) => {
-    //     <Link to={`/home/ques/${id}`} ></Link>
-    // }
+    
     
     return (
         <div>
-             {
+            <div className="details">
+            <h4>Ques Bank is mainly created for the Next Generation Coders.
+                Coders can test there knowledge by answering simply questions.They Can Master Code.
+            </h4>
+            <code>
+            Our Features 
+            <div className='features'>
+            <ol>
+                <li>We provide topic wise questions</li>
+                <li>User friendly</li>
+                <li>Easy answers</li>
+                <li>World Class Certification</li>
+            </ol>
+            </div>
+            </code>
+            </div>
+            <hr></hr>
+           <div>
+            <h2 className='topics'>Topics </h2>
+           
+           <div className="topicsContainer">
+           {
                 subjects.map(subject => <Subject
                 key={subject.id}
                 subject = {subject}
                
                 ></Subject>)
              }
+           </div>
+           </div>
         </div>
     );
 };
